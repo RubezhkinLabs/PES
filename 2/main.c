8e436392e1712228c6ba7d6338dff1043f3e3f9f
RubezhkinLabs <96119105+RubezhkinLabs@users.noreply.github.com>
2024-05-12 21:00:42 +0300

    Update main.c

--- 2/main.c
+++ 2/main.c
@@ -10,10 +10,7 @@ void led(void)
     PORT_LEDOUT &= ~(1<<LEDOUT_PIN); 
 }
 
-#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-ISR(PCINT1_vect, ISR_ALIASOF(PCINT0_vect));
-ISR(PCINT0_vect)
-{
+void hangle_interrupt(void){
   _delay_ms (50); // антидребезг (использовать задержки в прерываниях некошерно, но пока и так сойдёт)
   if (((PIN_B1 & (1<<B1_PIN)) == 0)&&((PIN_SWITCH & (1<<SWITCH_PIN)) == 0)) // если нажата одна из кнопок
   {
@@ -28,23 +25,16 @@ ISR(PCINT0_vect)
     while ( (PIN_B2 & (1<<B2_PIN)) == 0 ) {} // ждём отпускания кнопки
   } 
 }
-#else
 // Обработчик прерывания PCINT0
-ISR(PCINT_VEC0)
+ISR(PCINT0_vect)
 {
-  _delay_ms (50); // антидребезг (использовать задержки в прерываниях некошерно, но пока и так сойдёт)
-  if (((PIN_B1 & (1<<B1_PIN)) == 0)&&((PIN_SWITCH & (1<<SWITCH_PIN)) == 0)) // если нажата одна из кнопок
-  {
-    PORT_L1 ^= (1<<LED1_PIN); //переключаем состояние светодиода (вкл./выкл.)
-    led();
-    while ( (PIN_B1 & (1<<B1_PIN)) == 0 ) {} // ждём отпускания кнопки
-  }
-  if (((PIN_B2 & (1<<B2_PIN)) == 0)&&((PIN_SWITCH & (1<<SWITCH_PIN)) == 0)) // если нажата одна из кнопок
-  {
-    PORT_L2 ^= (1<<LED2_PIN); //переключаем состояние светодиода (вкл./выкл.)
-    led();
-    while ( (PIN_B2 & (1<<B2_PIN)) == 0 ) {} // ждём отпускания кнопки
-  } 
+  hangle_interrupt();
+}
+
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+ISR(PCINT1_vect)
+{
+  hangle_interrupt();
 }
 #endif
 
@@ -72,10 +62,19 @@ int main(void)
   PORT_LEDOUT &= ~(1<<LEDOUT_PIN); // выключен
 
   // Настройка прерываний
-  GIMSK |= (1<<PCIE_B1); // Разрешаем внешние прерывания PCINT0.
-  GIMSK |= (1<<PCIE_B2); // Разрешаем внешние прерывания PCINT0
-  PCMSK_B1 |= (1<<B1_PIN); // Разрешаем по маске прерывания на ногак кнопок (PCINT3, PCINT4)
-  PCMSK_B2 |= (1<<B2_PIN);
+  
+  #if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  GIMSK |= (1<<PCIE0); // Разрешаем внешние прерывания PCINT0.
+  PCMSK0 |= (1<<B1_PIN); // Разрешаем по маске прерывания на ногак кнопок (PCINT3, PCINT4)
+  PCMSK0 |= (1<<B2_PIN);
+  GIMSK |= (1<<PCIE1); // Разрешаем внешние прерывания PCINT0.
+  PCMSK1 |= (1<<B1_PIN); // Разрешаем по маске прерывания на ногак кнопок (PCINT3, PCINT4)
+  PCMSK1 |= (1<<B2_PIN);
+  #else
+  GIMSK |= (1<<PCIE); // Разрешаем внешние прерывания PCINT0.
+  PCMSK |= (1<<B1_PIN); // Разрешаем по маске прерывания на ногак кнопок (PCINT3, PCINT4)
+  PCMSK |= (1<<B2_PIN);
+  #endif
   sei(); // Разрешаем прерывания глобально: SREG |= (1<<SREG_I)
   while (1) 
   {
